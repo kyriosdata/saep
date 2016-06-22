99eb4ff76bd51dd079ae4d08bd015c5ce99f1c89
kyriosdata <fabio@engenhariadesoftware.inf.br>
2016-06-21 21:56:36 -0300

    modelo de dominio atualizado

---
+++ sandbox/dominio/src/main/java/br/ufg/inf/es/saep/sandbox/aplicacao/RelatorioParecerRequest.java
@@ -0,0 +1,14 @@
+package br.ufg.inf.es.saep.sandbox.aplicacao;
+
+import br.ufg.inf.es.saep.sandbox.dominio.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Produz relatório de um parecer.
+ */
+public class RelatorioParecerRequest {
+}
