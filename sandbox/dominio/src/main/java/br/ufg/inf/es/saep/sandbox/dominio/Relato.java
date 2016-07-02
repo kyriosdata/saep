33c5f04b2969f6bcdab27a267bdc0650321412d8
Fábio Nogueira de Lucena <fabio@engenhariadesoftware.inf.br>
2016-07-01 22:31:50 -0300

    refinamento do modelo

--- sandbox/dominio/src/main/java/br/ufg/inf/es/saep/sandbox/dominio/Relato.java
+++ sandbox/dominio/src/main/java/br/ufg/inf/es/saep/sandbox/dominio/Relato.java
@@ -6,44 +6,52 @@
 package br.ufg.inf.es.saep.sandbox.dominio;
 
 import java.util.Map;
+import java.util.Set;
 
 /**
  * Encapsula conjunto de valores que caracterizam
  * o relato de uma atividade ou produto.
- *
+ * <p>
  * Cada relato é de um tipo específico, que
  * representa um tipo de atividade ou tipo de
  * produto. Esse tipo é identificado por
  * {@link #tipo}.
- *
  */
 public class Relato implements Avaliavel {
     private String tipo;
-    private Map<String, Valor> valorPorNome;
+    private Map<String, Valor> valores;
 
     /**
      * Cria um relato a partir do tipo e valores correspondentes
      * fornecidos.
      *
-     * @param tipo O código do tipo do relato.
-     *
+     * @param tipo    O código do tipo do relato.
      * @param valores Conjunto byId valores para os tipos
      *                do relato.
      */
     public Relato(String tipo, Map<String, Valor> valores) {
+        if (tipo == null || tipo.isEmpty()) {
+            throw new CampoExigidoNaoFornecidoException("tipo");
+        }
+
+        if (valores == null || valores.size() == 0) {
+            throw new CampoExigidoNaoFornecidoException("valores");
+        }
+
         this.tipo = tipo;
-        valorPorNome = valores;
+        this.valores = valores;
     }
 
     /**
      * Recupera o valor do atributo.
      *
      * @param atributo O identificador único do atributo.
-     *
-     * @return O valor do atributo.
+     * @return O valor do atributo ou {@code null},
+     * caso o atributo não faça parte do relato.
      */
     public Valor get(String atributo) {
-        return valorPorNome.get(atributo);
+
+        return valores.get(atributo);
     }
 
     /**
@@ -57,20 +65,42 @@ public class Relato implements Avaliavel {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
+        if (this == o) {
+            return true;
+        }
 
-        Relato relato = (Relato) o;
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
 
-        return tipo.equals(relato.tipo)
-                && valorPorNome.equals(relato.valorPorNome);
+        Relato outro = (Relato) o;
+
+        if (!tipo.equals(outro.tipo)) {
+            return false;
+        }
+
+        Set<String> keys = valores.keySet();
+        Set<String> otherKeys = outro.valores.keySet();
+
+        if (keys.size() != otherKeys.size()) {
+            return false;
+        }
+
+        for (String key : keys) {
+            Valor esseValor = valores.get(key);
+            Valor outroValor = outro.valores.get(key);
+
+            if (!esseValor.equals(outroValor)) {
+                return false;
+            }
+        }
+
+        return true;
 
     }
 
     @Override
     public int hashCode() {
-        int result = tipo.hashCode();
-        result = 31 * result + valorPorNome.hashCode();
-        return result;
+        return tipo.hashCode();
     }
 }
---
+++ sandbox/dominio/src/test/java/br/ufg/inf/es/saep/sandbox/dominio/RelatoTest.java
@@ -0,0 +1,113 @@
+package br.ufg.inf.es.saep.sandbox.dominio;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
+
+public class RelatoTest {
+
+    Map<String, Valor> valores;
+    Map<String, Valor> vazio;
+
+    @Before
+    public void defineValores() {
+        valores = new HashMap<>(1);
+        valores.put("nome", new Valor(99f));
+
+        vazio = new HashMap<>(0);
+    }
+
+    @Test(expected = CampoExigidoNaoFornecidoException.class)
+    public void tipoNaoPodeSerNull() {
+        new Relato(null, valores);
+    }
+
+    @Test(expected = CampoExigidoNaoFornecidoException.class)
+    public void tipoNaoPodeSerVazio() {
+        new Relato("", valores);
+    }
+
+    @Test(expected = CampoExigidoNaoFornecidoException.class)
+    public void valoresNaoPodeSerNull() {
+        new Relato("tipo", null);
+    }
+
+    @Test(expected = CampoExigidoNaoFornecidoException.class)
+    public void valoresNaoPodeSerVazio() {
+        new Relato("tipo", vazio);
+    }
+
+    @Test
+    public void criaRecupera() {
+        Relato r = new Relato("a", valores);
+
+        assertEquals("a", r.getTipo());
+        assertEquals(99f, r.get("nome").getFloat(), 0.0001f);
+    }
+
+    @Test
+    public void relatosDistintosPorTipo() {
+        Relato r1 = new Relato("t1", valores);
+        Relato r2 = new Relato("t2", valores);
+
+        assertNotEquals(r1, r2);
+    }
+
+    @Test
+    public void relatoDistintoDeNull() {
+        Relato r1 = new Relato("t1", valores);
+        assertNotEquals(r1, null);
+    }
+
+    @Test
+    public void relatoIgualASi() {
+        Relato r1 = new Relato("t1", valores);
+        assertEquals(r1, r1);
+    }
+
+    @Test
+    public void relatoDistintoDeOutroTipoDeObjeto() {
+        Relato r1 = new Relato("t1", valores);
+        assertNotEquals(r1, "errado");
+    }
+
+    @Test
+    public void relatosDistintosPorValores() {
+        Relato r1 = new Relato("t", valores);
+
+        Map<String, Valor> outros = new HashMap<>(1);
+        outros.put("nome", new Valor(false));
+
+        Relato r2 = new Relato("t", outros);
+
+        assertNotEquals(r1, r2);
+    }
+
+    @Test
+    public void relatosDistintosPorQuantidadeDeValores() {
+        Relato r1 = new Relato("t", valores);
+
+        Map<String, Valor> outros = new HashMap<>(2);
+        outros.put("nome", new Valor(false));
+        outros.put("casa", new Valor(true));
+
+        Relato r2 = new Relato("t", outros);
+
+        assertNotEquals(r1, r2);
+    }
+
+    @Test
+    public void relatosIguaisHashCodeIguais() {
+        Relato r1 = new Relato("h", valores);
+        Relato r2 = new Relato("h", valores);
+
+        assertEquals(r1, r2);
+        assertEquals(r1.hashCode(), r2.hashCode());
+    }
+}
+
