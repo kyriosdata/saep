e5d29c813b77b62f7ec9275b6134adf234f3353a
Fábio Nogueira de Lucena <fabio@engenhariadesoftware.inf.br>
2016-09-12 21:16:02 -0300

    Alternativa de avaliador de expressao

---
+++ sandbox/dominio/src/test/java/br/ufg/inf/es/saep/sandbox/infraestrutura/Expressao.java
@@ -0,0 +1,10 @@
+/*
+ * Copyright (c) 2016. Fábrica de Software - Instituto de Informática (UFG)
+ * Creative Commons Attribution 4.0 International License.
+ */
+
+package br.ufg.inf.es.saep.sandbox.infraestrutura;
+
+public interface Expressao {
+    double valor();
+}
