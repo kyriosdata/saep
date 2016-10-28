62d4b98b1c25eb663f76449830914a786521e370
Fábio Nogueira de Lucena <fabio@engenhariadesoftware.inf.br>
2016-10-28 19:33:11 -0200

    documentacao

--- sandbox/dominio/src/main/java/br/ufg/inf/es/saep/sandbox/dominio/regra/package-info.java
+++ sandbox/dominio/src/main/java/br/ufg/inf/es/saep/sandbox/dominio/regra/package-info.java
@@ -1,5 +1,11 @@
 /**
- * Regra e casos específicos de regras, bem como serviços
- * oferecidos para gestão de regras, como configuração.
+ * Definição de regras disponíveis por meio das quais uma
+ * contagem sobre avaliáveis pode ser realizada.
+ *
+ * <p>Um conjunto de regras é definido por meio de uma configuração
+ * ({@link br.ufg.inf.es.saep.sandbox.dominio.regra.Configuracao}),
+ * que juntamente com o conjunto de avaliáveis, é suficiente para
+ * o processo de contagem depositar os resultados das avaliações das
+ * regras nas respectivas variáveis dessas regras.
  */
 package br.ufg.inf.es.saep.sandbox.dominio.regra;
