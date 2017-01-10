4204d192d80f51a2ce138a80daf34695356564ea
Fábio Nogueira de Lucena <fabio@engenhariadesoftware.inf.br>
2017-01-10 19:56:05 -0200

    inicio da ligação com regras

---
+++ sandbox/saep-cli/src/main/java/com/github/kyriosdata/saep/servicos/CalculaPontuacao.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2016 Fábio Nogueira de Lucena
+ * Fábrica de Software - Instituto de Informática (UFG)
+ * Creative Commons Attribution 4.0 International License.
+ */
+
+package com.github.kyriosdata.saep.servicos;
+
+import java.io.File;
+import java.util.Map;
+
+/**
+ * Encapsula serviço de cálculo de pontuação, o que inclui preparação
+ * dos dados para repassá-los para o domínio e, no sentido inverso,
+ * tradução para consumo externo.
+ */
+public class CalculaPontuacao {
+
+	public static Map<String, String> efetuaComputo(File cfg, File relatos) {
+	    return null;
+    }
+}
